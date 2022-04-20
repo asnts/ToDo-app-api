577a1cac64eb4b3efa0006236cc18d728a5d5b27
Annanda Santos <annandasants@gmail.com>
2022-04-19 22:39:57 -0300

    update tarefas.js

--- src/controllers/tarefas.js
+++ src/controllers/tarefas.js
@@ -1,23 +1,35 @@
 
 import {validatarefas, log} from "../services/validacoes.js"
+import DataBase from "../../infra/DataBase.js"
+import TarefasModel from "../models/tarefasModel.js"
+import res from "express/lib/response"
+
 
 
 class tarefas{
     static routers(app){
 
         app.get("/", (req, res) => {
-            if(validatarefas('tarefasDiarias')){
-                res.send('<h1> Rastreamento da aplicação sendo feito com nodemon </h1>')
+            // if(validatarefas('tarefasDiarias')){
+                res.send(DataBase)
 
 
-            }else{
-                res.send("<h1> Deu ruim </h1>")
-            }
+            // }else{
+            //     res.send("<h1> Deu ruim </h1>")
+            // }
+        })
+
+        app.get("/tarefas/", (req, res ) => {
+            const email = req.params.email
+            console.log(email)
+            res.status(200).json(obj.titulo)
+
         })
        
         app.post("/tarefas", (req, res ) => {
-            console.log(req.body)
-            res.send('<h1> Qualquer coisa </h1>')
+            const tarefa = new TarefasModel ((...Object.values (req.body))
+                DataBase.push(tarefa)
+            res.status(201).json(DataBase)
         })
 
     }
