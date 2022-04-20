f23359dd977b3141bead506378ceff5c8ac544ec
Annanda Santos <annandasants@gmail.com>
2022-04-19 22:55:57 -0300

    update TarefasModel.js

---
+++ src/models/tarefasModel.js
@@ -0,0 +1,13 @@
+class TarefasModel {
+    constructor(titulo, descricao, status, dataCriacao )
+{
+    this.titulo = titulo,
+    this.descricao = descricao,
+    this.status = status,
+    this.dataCriacao = dataCriacao
+
+}
+
+}
+
+export default TarefasModel
